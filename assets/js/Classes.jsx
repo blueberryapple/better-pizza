e6dad5a360ae4508c2abe12afb969b226cd1a61d
Ben <teabens@outlook.com>
2017-12-22 12:47:40 -0800

    made basic components to test routing and mockapi

---
+++ assets/js/Classes.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link, Route } from 'react-router-dom'
+import getClasses from './mockApis/getClasses'
+
+export default function Classes(){
+    const classes = getClasses().map((cl) => {
+        return (
+            <li key={cl}>
+                <Link to={cl}>
+                    { cl }
+                </Link>
+            </li>
+        )
+    })
+
+    return (
+        <div>
+            <ul>
+                { classes }
+            </ul>
+        </div>
+    )
+}
