532c9fafe0f058732f9a57b4643202317314e88d
Ben Tea <9259833+blueberryapple@users.noreply.github.com>
2017-12-22 16:13:00 -0800

    catch all url

--- forum/urls.py
+++ forum/urls.py
@@ -5,4 +5,5 @@ app_name = 'forum'
 urlpatterns = [\
     re_path(r'^$', views.forum, name='index'),
     re_path(r'^logout', views.logout, name='logout'),
+    re_path(r'^', views.forum, name='all-else'), # catches all other queries and sends it to the forum view
 ]
