9e6236df6fdc136d0ca14c46d5639d29a6cf94ee
Ben Tea <9259833+blueberryapple@users.noreply.github.com>
2017-12-22 16:14:13 -0800

    reorganized components

--- assets/js/Posts/Posts.jsx
+++ assets/js/Posts/Posts.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import getPosts from './mockApis/getPosts'
+import getPosts from '../mockApis/getPosts'
+import css from './post.css'
 
 export default function Posts({ match }) {
     const posts = getPosts().map((post) => {
         return (
-            <span key={post.id}>
+            <div key={post.id} className='post'>
                 <p>{ post.title }</p>
                 <p>{ post.body }</p>
-            </span>
+            </div>
         )
     })
 
