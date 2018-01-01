62c5deed5b6f0242763a01c7eab69a01801347a4
Ben <teabens@outlook.com>
2018-01-01 10:29:22 -0800

    removed landing

--- login/views.py
+++ login/views.py
@@ -4,9 +4,5 @@ from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-@login_required
-def landing(request):
-    return render(request, 'login/landing.html')
-
 def logout(request):
     return logout_then_login(request, login_url='login')
